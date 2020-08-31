56a6bd0a77e6049407b8877f3a58b5edde62ea08
unknown <andrew.shakalov@gmail.com>
2020-08-31 15:45:45 +0300

    404

--- routes/404.js
+++ routes/404.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET 404 listing. */
-router.get('/', function(req, res, next) {
+router.get('https://fierce-reaches-84796.herokuapp.com/', function(req, res, next) {
   res.render('404');
 });
 
